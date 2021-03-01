14dea33c9f682c5c2aec6b1321829777e1501c70
parisdolfman <parisdolfman@gmail.com>
2021-03-01 14:58:59 -0500

    Added config.ru

---
+++ config/environment.rb
@@ -0,0 +1,9 @@
+ENV['SINATRA_ENV'] ||= "development"
+
+require 'bundler/setup'
+require 'capybara/dsl'
+Bundler.require(:default, ENV['SINATRA_ENV'])
+
+ActiveRecord::Base.establish_connection(
+  :adapter => "sqlite3",
+  :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
