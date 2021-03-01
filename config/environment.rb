58d78c91ffeef913011dada1c3cbb56e510c0648
parisdolfman <parisdolfman@gmail.com>
2021-03-01 15:41:41 -0500

    Fixed missing end of input

--- config/environment.rb
+++ config/environment.rb
@@ -7,6 +7,6 @@ Bundler.require(:default, ENV['SINATRA_ENV'])
 ActiveRecord::Base.establish_connection(
   :adapter => "sqlite3",
   :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
-
+)
 
   # need a require_all here for controllers in app and 
