9c8a6c4ad19d6eab8b2a52b7afd35d3a93cb184f
parisdolfman <parisdolfman@gmail.com>
2021-03-02 13:44:06 -0500

    Changed location of views:

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,16 +2,16 @@ require './config/environment'
 
 class ApplicationController < Sinatra::Base
 
-    configure do
-        set :public_folder, 'public'
-        set :views, 'app/views'
-        enable :sessions
-        set :session_secret, "extra_secret"
-      end
+   configure do
+       set :public_folder, 'public'
+       set :views, 'app/views'
+       enable :sessions
+       set :session_secret, "extra_secret"
+     end
 
     get '/' do
       erb :index
-    end 
+    end
 
 
   helpers do
