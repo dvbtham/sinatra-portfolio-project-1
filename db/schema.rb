aead88d77b1938129fc91a0d1aeb46d5250bde3d
parisdolfman <parisdolfman@gmail.com>
2021-03-02 14:01:25 -0500

    Re-migrated paintings, artists and users. New schema produced

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_02_155658) do
+ActiveRecord::Schema.define(version: 2021_03_02_162821) do
 
   create_table "artists", force: :cascade do |t|
     t.string "name"
