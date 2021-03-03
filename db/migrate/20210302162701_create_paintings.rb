9702face82794a69893565aba9c7675e07fc28c2
parisdolfman <parisdolfman@gmail.com>
2021-03-03 11:35:47 -0500

    Removed create_paintings in order to remigrate due to error

--- db/migrate/20210302162701_create_paintings.rb
+++
@@ -1,9 +0,0 @@
-class CreatePaintings < ActiveRecord::Migration[6.0]
-  def change
-    create_table :paintings do |t|
-      t.string :name
-      t.string :artist_name
-      t.string :year
-    end 
-  end
-end
