b7df4ee02f55ca5221b0d2fbb80dbc7feac89426
parisdolfman <parisdolfman@gmail.com>
2021-03-03 11:37:05 -0500

    Schema updated with new  migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_02_162821) do
+ActiveRecord::Schema.define(version: 2021_03_03_151621) do
 
   create_table "artists", force: :cascade do |t|
     t.string "name"
   end
 
   create_table "paintings", force: :cascade do |t|
-    t.string "name"
-    t.string "artist_name"
+    t.string "painting_name"
     t.string "year"
+    t.integer "artist_id"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
