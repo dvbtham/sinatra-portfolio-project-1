31da4587c6cced2eabc10129c8727b68c657044c
parisdolfman <parisdolfman@gmail.com>
2021-03-03 12:17:14 -0500

    Created seed file

---
+++ db/seeds.rb
@@ -0,0 +1,11 @@
+Artist.create(name: "Pablo Picasso")
+Artist.create(name: "Claude Monet")
+Artist.create(name: "Vincent van Gogh")
+
+
+Painting.create(painting_name: "Guernica", year: "1937", artist_id: 1, user_id: 1)
+Painting.create(painting_name: "Impression Sunrise", year: "1872", artist_id: 2, user_id: 1)
+Painting.create(painting_name: "The Starry Night", year: "1889", artist_id: 3, user_id: 1)
+
+
+puts "data loaded success"
