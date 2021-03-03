be5901bca990f4e5df85c63545c4d37e8086be78
parisdolfman <parisdolfman@gmail.com>
2021-03-03 12:14:29 -0500

    Added slug method

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
 
+  get '/users/:slug' do
+    @user = User.find_by_slug(params[:slug])
+    erb :'users/user_homepage'
+  end
+
 
   get '/login' do
     if !logged_in?
@@ -13,10 +18,18 @@ post '/login' do
     user = User.find_by(:username => params[:username])
     if user && user.authenticate(params[:password])
         session[:user_id] = user.id
-        redirect to '/'
+        redirect to '/user_homepage'
     else 
         redirect to '/'
     end 
+end 
+
+get '/user_homepage' do
+  if logged_in?
+    erb :'users/user_homepage'
+  else
+    redirect '/'
+  end
 end 
 
 
