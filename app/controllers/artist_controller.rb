517d7a26af34c47d7cd39cffdb8fa71834264b40
parisdolfman <parisdolfman@gmail.com>
2021-03-07 10:18:23 -0500

    Created artist :id get method

--- app/controllers/artist_controller.rb
+++ app/controllers/artist_controller.rb
@@ -7,7 +7,24 @@ class ArtistController < ApplicationController
     
     get "/artists/new" do
       erb :'artists/new'
-    end    
+    end
+    
+    get "/artists/:id/edit" do
+        @artist = Artist.find(params[:id])
+        erb :'artists/edit'
+      end
+
+
+      post "/artists/:id" do
+        @artist = Artist.find(params[:id])
+        @artist.update(params.select{|a| a=="name")
+        redirect "/artists/#{@artist.id}"
+      end
+
+    get "/artists/:id" do
+      @artist = Artist.find(params[:id])
+      erb :'artists/show'
+    end
 
 
 
