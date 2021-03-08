93d04f536043d893c485f2fb8271af3dc8257afb
parisdolfman <parisdolfman@gmail.com>
2021-03-08 08:18:13 -0500

    fixing paintings controller

--- app/controllers/paintings_controller.rb
+++ app/controllers/paintings_controller.rb
@@ -3,35 +3,35 @@ class PaintingsController < ApplicationController
     get '/paintings' do
         if logged_in?
           @paintings = Painting.all
-          erb :'paintings/paintings'
+          erb :'paintings/index'
         else
           redirect to '/login'
         end 
       end
 
-    post "/paintings" do
+    post "/paintings/new" do  #create post
       Painting.create(params)
-      redirect "/paintings"
+      redirect "/user_homepage"
      end
 
-    get "/paintings/new" do
+    get "/new" do  #create get
       erb :'paintings/new'
     end  
 
 
-    get "/paintings/:id/edit" do
+    get "/paintings/:id/edit" do #update get
         @painting = Painting.find(params[:id])
         erb :'paintings/edit'
     end
 
-    post "/paintings/:id" do
+    post "/paintings/:id" do #update post
         @painting = Painting.find(params[:id])
         @painting.update(params.select{|p| p=="name" || p=="year" || p=="artist_id"})
         redirect "/paintings/#{@painting.id}"
       end
 
 
-      get "/paintings/:id" do
+      get "/paintings/:id" do #read get
         @painting = Painting.find(params[:id])
         erb :'paintings/show'
       end
