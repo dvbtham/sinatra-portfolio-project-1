de6eba158d62cd3970110e0533b4e0fa047ba5ce
parisdolfman <parisdolfman@gmail.com>
2021-03-12 15:54:33 -0500

    Fixed /new method to direct to user homepage. Method was causing error of no get for homepage

--- app/controllers/paintings_controller.rb
+++ app/controllers/paintings_controller.rb
@@ -1,6 +1,6 @@
 class PaintingsController < ApplicationController
 
-  get '/index' do
+  get '/index' do #read get
     if logged_in?
       @paintings = Painting.all
       erb :'paintings/index'
@@ -13,29 +13,37 @@ class PaintingsController < ApplicationController
     erb :'paintings/new'
   end 
 
-    post "/new" do  #create post
+  post "/new" do  #create post
       Painting.create(params)
       redirect "/user_homepage"
-     end
+  end
+
+  # post '/index' do #read post
+  #   if logged_in?
+  #     if params[:name] == ""
+  #       redirect to "/new"
+  #     else
+  #       @painting = current_user.paintings.build(name: params[:name])
+  #       if @painting.save
+  #         redirect to "/painting/#{@painting.id}"
+  #       else
+  #         redirect to "/new"
+  #       end
+  #     end
+  #   else
+  #     redirect to '/login'
+  #   end
+  # end
 
 
-
-   post '/user_homepage' do #changed from /index to /user_homepage. Did not fix.
-     if logged_in?
-       if params[:name] == ""
-         redirect to "/new"
-       else
-         @painting = current_user.paintings.build(name: params[:name])
-         if @painting.save
-           redirect to "/index/#{@painting.id}"
-         else
-           redirect to "/new"
-         end
-       end
-     else
-       redirect to '/login'
-     end
-   end  
+  #   get '/paintings/:id' do
+  #     if logged_in?
+  #       @painting = Painting.find_by_id(params[:id])
+  #       erb :'paintings/show'
+  #     else
+  #       redirect to '/index'
+  #     end
+  #   end
 
 
 
@@ -43,7 +51,7 @@ class PaintingsController < ApplicationController
       if logged_in?
           @painting = Painting.find_by_id(params[:id])
         if @painting && @painting.user == current_user
-          erb :'paintings/edit_tweet'
+          erb :'paintings/edit'
         else
         redirect to '/user_homepage'
         end
@@ -52,25 +60,44 @@ class PaintingsController < ApplicationController
       end
     end
 
-    post "/paintings/:id" do #update post
-        @painting = Painting.find(params[:id])
-        @painting.update(params.select{|p| p=="name" || p=="year" || p=="artist_id"})
-        redirect "/paintings/#{@painting.id}"
-      end
 
-
-      get "/paintings/:id" do 
-        @painting = Painting.find(params[:id])
-        erb :'paintings/show'
-      end
-
-      get '/user_homepage' do
-        if logged_in?
-          erb :'users/user_homepage'
+    patch '/paintings/:id/edit' do
+      if logged_in?
+        if params[:name] == ""
+          redirect to "/paintings/#{params[:id]}/edit"
         else
-          redirect '/index'
+          @painting = Painting.find_by_id(params[:id])
+          @user = User.find_by_id(session[:user_id])
+          if @painting && @painting.user == current_user
+            if @painting.update(content: params[:content])
+              redirect to "/paintings/#{@painting.id}"
+            else
+              redirect to "/paintings/#{@painting.id}/edit"
+            end
+          else
+            redirect to '/index'
+          end
         end
-      end 
+      else
+        redirect to '/login'
+      end
+    end
+
+    # post "/paintings/:id" do #update post
+    #     @painting = Painting.find(params[:id])
+    #     @painting.update(params.select{|p| p=="name" || p=="year" || p=="artist_id"})
+    #     redirect "/paintings/#{@painting.id}"
+    #   end
+
+
+
+      # get '/user_homepage' do
+      #   if logged_in?
+      #     erb :'users/user_homepage'
+      #   else
+      #     redirect '/index'
+      #   end
+      # end 
       
 
 
