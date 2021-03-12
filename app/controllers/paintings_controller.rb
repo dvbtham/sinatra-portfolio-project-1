d365ae1c2787f2ae378a7227b7ef2e0999fb1c03
parisdolfman <parisdolfman@gmail.com>
2021-03-12 16:49:59 -0500

    Added delete method

--- app/controllers/paintings_controller.rb
+++ app/controllers/paintings_controller.rb
@@ -13,27 +13,27 @@ class PaintingsController < ApplicationController
     erb :'paintings/new'
   end 
 
-  post "/new" do  #create post
-      Painting.create(params)
-      redirect "/user_homepage"
-  end
-
-  # post '/index' do #read post
-  #   if logged_in?
-  #     if params[:name] == ""
-  #       redirect to "/new"
-  #     else
-  #       @painting = current_user.paintings.build(name: params[:name])
-  #       if @painting.save
-  #         redirect to "/painting/#{@painting.id}"
-  #       else
-  #         redirect to "/new"
-  #       end
-  #     end
-  #   else
-  #     redirect to '/login'
-  #   end
+  # post "/new" do  #create post
+  #     Painting.create(params)
+  #     redirect "/user_homepage"
   # end
+
+  post '/index' do 
+    if logged_in?
+      if params[:name] == ""
+        redirect to "/new"
+      else
+        @painting = current_user.paintings.build(name: params[:name])
+        if @painting.save
+          redirect to "/paintings/#{@painting.id}"
+        else
+          redirect to "/new"
+        end
+      end
+    else
+      redirect to '/login'
+    end
+  end
 
 
     get '/paintings/:id' do
@@ -53,7 +53,7 @@ class PaintingsController < ApplicationController
         if @painting && @painting.user == current_user
           erb :'paintings/edit'
         else
-        redirect to '/user_homepage'
+        redirect to '/index'
         end
       else
         redirect to '/login'
@@ -61,15 +61,14 @@ class PaintingsController < ApplicationController
     end
 
 
-    patch '/paintings/:id/edit' do
+    patch '/paintings/:id' do
       if logged_in?
         if params[:name] == ""
           redirect to "/paintings/#{params[:id]}/edit"
         else
           @painting = Painting.find_by_id(params[:id])
-          @user = User.find_by_id(session[:user_id])
           if @painting && @painting.user == current_user
-            if @painting.update(content: params[:content])
+            if @painting.update(name: params[:name])
               redirect to "/paintings/#{@painting.id}"
             else
               redirect to "/paintings/#{@painting.id}/edit"
@@ -83,27 +82,18 @@ class PaintingsController < ApplicationController
       end
     end
 
-    # post "/paintings/:id" do #update post
-    #     @painting = Painting.find(params[:id])
-    #     @painting.update(params.select{|p| p=="name" || p=="year" || p=="artist_id"})
-    #     redirect "/paintings/#{@painting.id}"
-    #   end
-
-
-
-      # get '/user_homepage' do
-      #   if logged_in?
-      #     erb :'users/user_homepage'
-      #   else
-      #     redirect '/index'
-      #   end
-      # end 
-      
-
-
-
-
-   
+    delete '/paintings/:id/delete' do
+      if logged_in?
+        @painting = Painting.find_by_id(params[:id])
+        if @painting && @painting.user == current_user
+          @painting.delete
+        end
+        redirect to '/index'
+      else
+        redirect to '/login'
+      end
+    end    
+ 
 
 
 end 
